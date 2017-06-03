15de42d81e683440684bf0d7b435f2c558bd8993
Scott Lanning <slanning@cpan.org>
2017-06-03 20:27:39 +0200

    trying to implement http://www.cs.nuim.ie/~matthewh/VST.html but it decays way too fast, sounds like a drum

---
+++ synth/karplus-strong-extended.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+# http://www.cs.nuim.ie/~matthewh/VST.html
+use strict;
+use warnings;
+use Data::Dumper; { package Data::Dumper; our ($Indent, $Sortkeys, $Terse, $Useqq) = (1)x4 }
+
+my $SAMPLE_RATE      = 44100;   # samples per second, Hz
+
+my $BYTES_PER_SAMPLE = 2;
+my $packfmt = ($BYTES_PER_SAMPLE == 2)
+  ? 's<'   # signed 16-bit little-endian (I think 'v' also works)
+  : ($BYTES_PER_SAMPLE == 1)
+    ? 'C'  # unsigned 8-bit
+    : die("unsupported BYTES_PER_SAMPLE\n");
+my $MAX_AMPLITUDE = 2**(8*$BYTES_PER_SAMPLE - 1) - 1;  # signed
+
+my ($freq, $sec) = @ARGV;
+$freq  //= 261.626;  # middle C
+$sec   //= 2.5;      # time
+
+# here the signal is assumed to be normalized (-1,1);
+# later it gets scaled to $MAX_AMPLITUDE
+
+# 1. excitation and delay line
+my $N = my $delta = int(($SAMPLE_RATE / $freq) - 0.5 - 0.001);
+my @r = map(2 * (rand(1) - 0.5), 1 .. $N);
+my @d = (0) x $N;
+
+# 2. loss and tuning filter
+my $t = (1 - $delta) / (1 + $delta);   # fractional delay
+my $tau = $sec;   # time in seconds where waveform loses 60dB; arbitrary here
+my $g = 10**(-6 / ($freq * $tau));
+my $b0 = 0.5 * $g * $t;
+my $b1 = 0.5 * $g * ($t + 1);   # I assumed (1/g)g(t+1) was a typo (?)
+my $b2 = 0.5 * $g;
+my $a = $t;
+my $f0 = 0;  # not sure how to initialize these
+my $f1 = 0;
+
+# 3. velocity filter: -1 < v <= 0
+my $vel = 120;   # 0 - 127 (midi), arbitrary here
+my $vmin = 0.01;   # should be a "small" value
+my $v = $vmin**(1 - $vel / 128) - 1;
+my $w = 0;
+
+# 4. plucking-point filter: 0 < u <= 0.5; 0.5 = half-string point (flute-like)
+my $u = 0.5;  # arbitrary
+my $P = (int($u*$N) > 1) ? int($u*$N) : 1;   # max(int(uN), 1)
+my $wt = 2500;   # arbitrary, but see the link "best results"
+my $rho = sqrt((2 * $u * $N * $wt) / ($u * $N * $wt - sin($u * $N * $wt)));
+my @q = (0) x $P;   # initialized to zero?
+
+my $num_samples = $SAMPLE_RATE * $sec;
+foreach my $n (0 .. $num_samples - 1) {
+    my $y;
+    if ($n < $N) {
+        $y = $r[$n];
+    }
+    else {
+        my $nmod = $n % $N;
+        my $x = $d[$nmod];
+        $y = F($x);
+        $d[$nmod] = $y;
+    }
+
+    print pack($packfmt, int($MAX_AMPLITUDE * V(P($y, $n))));
+}
+
+sub F {
+    my ($x) = @_;
+
+    my $y = $b0 * $x + $f0;
+    $f0 = $b1 * $x + $f1 - $a * $y;
+    $f1 = $b2 * $x;
+
+    return($y);
+}
+
+sub V {
+    my ($y) = @_;
+
+    $w = (1 + $v) * $y - $v * $w;
+
+    return($w);
+}
+
+sub P {
+    my ($y, $n) = @_;
+
+    my $pmod = $n % $P;
+    my $z = $rho * ($y - $q[$pmod]);
+    $q[$pmod] = $y;
+
+    return($z);
+}
+
+__END__
